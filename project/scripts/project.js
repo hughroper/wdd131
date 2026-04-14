21b7418608a6b3b77bf28328f08fc6fa2315518d
hughroper <hughroper@icloud.com>
2026-04-13 22:30:35 -0600

    project updates

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,21 +1,28 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const year = new Date().getFullYear();
+document.getElementById("currentYear").textContent = year;
+
+
+
 const vegetables = [
     { id: "carrots", name: "Carrots", seeds: 16, method: "Canning", yield: "0.5", type: "jars" },
-    { id: "broccoli", name: "Broccoli", seeds: 1, method: "Freeze", yield: "1.0", type: "bags" },
+    { id: "broccoli", name: "Broccoli", seeds: 1, method: "Freezer", yield: "1.0", type: "bags" },
     { id: "cabbage", name: "Cabbage", seeds: 1, method: "Ferment", yield: "1.0", type: "jars" },
-    { id: "cauliflower", name: "Cauliflower", seeds: 1, method: "Freeze", yield: "1.0", type: "bags" },
+    { id: "cauliflower", name: "Cauliflower", seeds: 1, method: "Freezer", yield: "1.0", type: "bags" },
     { id: "lettuce", name: "Lettuce", seeds: 4, method: "Eat fresh", yield: "1-1.5 lbs", type: "Fresh Produce" },
-    { id: "spinach", name: "Spinach", seeds: 9, method: "Freeze", yield: "0.5", type: "bags" },
-    { id: "kale", name: "Kale", seeds: 1, method: "Freeze", yield: "1.0", type: "bags" },
-    { id: "peas", name: "Peas", seeds: 9, method: "Freeze", yield: "0.5", type: "bags" },
+    { id: "spinach", name: "Spinach", seeds: 9, method: "Freezer", yield: "0.5", type: "bags" },
+    { id: "kale", name: "Kale", seeds: 1, method: "Freezer", yield: "1.0", type: "bags" },
+    { id: "peas", name: "Peas", seeds: 9, method: "Freezer", yield: "0.5", type: "bags" },
     { id: "radishes", name: "Radishes", seeds: 16, method: "Pickle", yield: "0.5", type: "jars" },
     { id: "beets", name: "Beets", seeds: 9, method: "Canning", yield: "0.5", type: "jars" },
     { id: "tomatoes", name: "Tomatoes", seeds: 1, method: "Canning", yield: "2.5-3.0", type: "jars" },
-    { id: "corn", name: "Sweet corn", seeds: 4, method: "Freeze", yield: "0.5-1.0", type: "bags" },
+    { id: "corn", name: "Sweet corn", seeds: 4, method: "Freezer", yield: "0.5-1.0", type: "bags" },
     { id: "beans", name: "Green beans", seeds: 9, method: "Canning", yield: "0.5", type: "jars" },
     { id: "cucumbers", name: "Cucumbers", seeds: 4, method: "Pickle", yield: "2.0-3.0", type: "jars" },
-    { id: "zucchini", name: "Zucchini squash", seeds: 1, method: "Freeze", yield: "2.0", type: "bags" },
-    { id: "peppers", name: "Bell peppers", seeds: 1, method: "Freeze", yield: "1.0-2.0", type: "bags" },
-    { id: "eggplant", name: "Eggplant", seeds: 1, method: "Freeze", yield: "1.0-2.0", type: "bags" },
+    { id: "zucchini", name: "Zucchini squash", seeds: 1, method: "Freezer", yield: "2.0", type: "bags" },
+    { id: "peppers", name: "Bell peppers", seeds: 1, method: "Freezer", yield: "1.0-2.0", type: "bags" },
+    { id: "eggplant", name: "Eggplant", seeds: 1, method: "Freezer", yield: "1.0-2.0", type: "bags" },
     { id: "onions", name: "Onions", seeds: 16, method: "Cure", yield: "1 lb", type: "Dry Storage" },
     { id: "garlic", name: "Garlic", seeds: 9, method: "Cure", yield: "1-1.5 lbs", type: "Dry Storage" }
 ];
